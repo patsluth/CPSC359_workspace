743a63daa92f6e4cd6562ea0e43af4ad98977052
nescando <nescando@pi02-wc.cs.ucalgary.ca>
2016-10-20 12:11:41 -0600

    Updated with changes from Wednesday night

--- a2/source/main.s
+++ a2/source/main.s
@@ -1,5 +1,5 @@
 .section    .init
-.globl     _start
+.global     _start
 
 _start:
     b       main
@@ -10,25 +10,9 @@ main:
 	mov sp, #0x8000               // Initializing the stack pointer
 	bl EnableJTAG                 // Enable JTAG
 	bl InitUART                   // Initialize the UART
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
 	ldr r1, =inputBuffer
   ldrb r0, [r1]
 
@@ -60,64 +44,70 @@ getNumberListSize:
   //NJE: Make sure that it's within [1-9].
   // Otherwise, branch to get number list again.
 
+  // after calling readLineUART, r0 holds length of input buffer (I THINK??)
   // if r0 != 1, wrong format.
   cmp r0, #1
   bne wrongListFormat
 
-  // Otherwise, continue with checks
+  // Otherwise, load value from input buffer, continue with checks
   ldr r4, =inputBuffer
   ldrb r0, [r4]
 
-  // if r0 (ascii value) < 48 (ascii for 0), wrong format
-  cmp r0, #48
-  bl  wrongListFormat
+  // if r0 (ascii value) < 489 (ascii for 1), wrong format
+  cmp r0, #49
+  blt  wrongListFormat
 
   // if r0 (ascii value) > 57 (ascii for 9), wrong format
   cmp r0, #57
-  bl  wrongListFormat
+  bgt  wrongListFormat
+
+
+//TODO
+
+//If we make it this far, the value should be an integer from 1-9
+  //Save this value to a register/array for later use.
+
+//Need to build the main loop tonight
 
 
 
   //NJE: This is how to load
-  ldr r4, =inputBuffer
-  ldrb r5, [r4]
-  ldrb r6, [r4, #1]
-  mov r1, r1
-  nop
-
-  //First, take in all numbers
+  //ldr r4, =inputBuffer
+  //ldrb r5, [r4]
+  //ldrb r6, [r4, #1]
+  //mov r1, r1
+  //nop
   //
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -134,72 +124,72 @@ getNumberListSize:
 	ldr r0, =newline
 	mov	r1, #1
 	bl WriteStringUART
-	
+
 	// Print new line
 	ldr r0, =newline
 	mov	r1, #1
 	bl WriteStringUART
-	
+
 	// Print new line
 	ldr r0, =newline
 	mov	r1, #1
 	bl WriteStringUART
-	
+
 	// Print new line
 	ldr r0, =newline
 	mov	r1, #1
 	bl WriteStringUART
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	// Sort array
 	ldr r0, =testArray
 	ldr r1, =testArrayEnd
 	bl sortArray
-	
-	
-	
-	
+
+
+
+
 	// Get median value of sorted array
 	ldr r0, =testArray
 	ldr r1, =testArrayEnd
 	bl getMedian
-	
-	
+
+
 
 
 	// decimal to asci
-	
+
 	ldr r0, =decToASCIBuffer
 	mov r3, r2
 	mov r4, #10
-	
+
 	// iterate digits
 	// let r2 = n
 	// let r3 = n copy
-	
+
 	loooooop:
-	
+
 		udiv r3, r3, r4			// n /= 10
 		mul r5, r3, r4			// r3 * 10
 		sub r5, r2, r5			// n - (n / 10)
 		// r5 = (n % 10) at this point
-		
+
 		strb r5, [r0]
 		ldrb r7, [r0]
-		
+
 		mov r2, r3				// update n
-		
+
 		cmp r2, #0				// if (n <= 0) then done
-		
+
 		ble donelooooop
-		
-		
+
+
 		b loooooop
-		
+
 	donelooooop:
 
 
@@ -213,14 +203,14 @@ getNumberListSize:
 
 
 
-	
+
 	ldr r0, =testArray
 	ldr r1, =testArrayEnd
 	sub	r1, r0						// r1 = length of testArray
 	bl WriteStringUART
 
 
-	
+
 
 
 	b stop
@@ -280,19 +270,19 @@ mov r2, r0      // save reference to arrayStart
 		strb r4, [r0]
 		mov r0, r2          		// reset r0 to arrayStart
 		b loopBody
-	
+
 	loopEnd:
 
 	mov pc, r14			 			// return
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
 // input r0 = arrayStart
 // input r1 = arrayEnd
 // output r2  = median value
@@ -305,13 +295,13 @@ mov r2, r0      // save reference to arrayStart
 	add r0, #1						// advance array start 1 index
 	sub r1, #1						// advance array end -1 index
 
-    cmp r0, r1           			
+    cmp r0, r1
     bge loopEnd_					// if (start index >= end index) then found mid
 
     b loopBody_
 
   loopEnd_:
-  
+
 	// if the array size is even, r1 will be r0 - 1
 	// if the array size is odd, r1 will equal r0
 	ldrb r2, [r1]
@@ -320,8 +310,8 @@ mov r2, r0      // save reference to arrayStart
 
 
 
-  
-  
+
+
 
 
 
