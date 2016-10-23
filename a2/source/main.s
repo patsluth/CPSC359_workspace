1b338f89ce436e93ab86df848ac8126f378a8db2
nescando <nescando@pi01-eb.cs.ucalgary.ca>
2016-10-23 16:45:32 -0600

    Finished writing main loop and ascii to dec conversion. Need to test.

--- a2/source/main.s
+++ a2/source/main.s
@@ -12,43 +12,6 @@ main:
 	bl InitUART                   // Initialize the UART
 
 
-
-  //NJE: This is how to load
-  //ldr r4, =inputBuffer
-  //ldrb r5, [r4]
-  //ldrb r6, [r4, #1]
-  //mov r1, r1
-  //nop
-
-
-
-
-
-  //NOTE: THIS WORKS NOW. THIS IS HOW TO READ FROM ARRAY
-      //I just don't know how to access individual elements.
-        //Think it has to do with the "This is how to load"
-  //ldr r4, =loopNumberString
-  //mov r0, r4
-  //mov r1, #7
-  //bl WriteStringUART
-
-
-  bl stop
-
-  //ldr r4, =loopNumberString
-  //ldrb r0, [r4, #1]
-  //mov r1, #6
-  //bl WriteStringUART
-
-  //ldr r4, =loopNumberSize
-  //ldrb r1, [r4, #4]
-  //bl WriteStringUART
-  //nop
-  //nop
-  //nop
-  //ENDTODO
-
-
   //NJE: Print names of creators
   ldr r0, =creatorString
   mov r1, #47
@@ -66,7 +29,7 @@ wrongListFormat:
   ldr r0, =wrongListSizeFormatString
   mov r1, #50
   bl  WriteStringUART
-  //NJE: Get size of number list
+
 
 getNumberListSize:
   ldr r0, =inputBuffer
@@ -95,12 +58,10 @@ getNumberListSize:
   bgt  wrongListFormat
 
 
-
-
-  //If we make it this far, value should be good.
-
-  //NJE TODO: Subtract 48 from r0 to convert to decimal
+  //If we make it this far, value for list size should be good.
+  //Subtract 48 from r0 to convert to decimal
   //Save this into r12
+  sub r0, r0, #48
   mov r12, r0
 
 
@@ -110,10 +71,6 @@ getNumberListSize:
   //Setup for while loop//
   //NOTES: r11 will be used to store iteration counter
   mov r11, #0
-
-  //Beginning of the while r11 < r12 loop
-  bl  test
-  nop
 
 
 test:
@@ -127,12 +84,13 @@ badInt:
 
   //TODO: Print that it's the wrong input.
 
+
 mainLoop:
 
   //This is where the stuff happens for the main loop
 
 
-    //NJE TODO: Write "Please enter xth number"
+    //Write "Please enter xth number"
     ldr r0, =inputRequest   //"Please enter the "
     mov r1, #17
     bl WriteStringUART
@@ -153,16 +111,19 @@ mainLoop:
 intInput:
 
     ldr r0, =inputBuffer
-    mov r1, #4
+    mov r1, #4            //reading 4 characters. Up to 3 numbers + \n character
     bl  ReadLineUART
     nop
 
 
 //------------------------------------------------------//
-    //NJE TODO: Check input
+    //CHECKING INPUT
+    //After ReadLine, r0 holds the number of characters read in
+    //r8 is my result (converting from ascii string to decimal)
     //r9 is my iterator
     //r10 used to keep track of how many elements to look at
 
+    mov r8, #0
     mov r9, #0
     mov r10, r0
 
@@ -185,30 +146,70 @@ intInput:
       cmp r0, #57
       bgt  badInt
 
+      //if r10 == 1, we can just convert by subtracting 48
+      cmp r10, #1
+      bne atoi
 
+      sub r8, r0, #48
+      bl endOfIteration
+
+
+      atoi:
+        //convert to decimal, add it to result (r8)
+        //num places is r10 - r9
+        sub r7, r10, r9
+        mov r6, #0
+        mov r5, #1          //r5 is multiplication factor
+        mov r4, #10         //I guess multiplication can't take constants??
+
+
+      expLoop:
+        //get multiplication factor (r5) by multiplying r5 by 10, r7 times
+        cmp r6, r7
+        bge afterExpLoop
+
+        //while r6 < r7
+        mul r5, r5, r4
+        add r6, r6, #1  //increment r6
+
+      afterExpLoop:
+
+        sub r0, r0, #48   //convert to int value
+        mul r0, r0, r5    //raise it to the appropriate power
+        add r8, r8, r0    //add it to my result
+        bl endOfIteration
+
+
+
+
+
+      endOfIteration:
 
       add r9, r9, #1    //increment r9
       bl intTest
 
+      //If it gets here, the value is good and can be stored
 
 
-    //If it gets here, the value is good and can be stored
     doneIntTest:
 
+      //NJE TODO: Save value in r8 array
+      //Store value in r8 into the array (atodArray), offset by r9
 
-    //NJE TODO: Convert from ascii to int
-    //NJE TODO: Save value to array
+      ldr r0, =atodArray
+      ldr r1, =atodArrayEnd
+      sub r1, r0
+
+      strb r8, [r0, r9]
 
 
-  //end of loop
-  add r11, r11, #1       //increment r11
-  bl test     //We always want to jump back to test
+      //end of loop
+      add r11, r11, #1       //increment r11
+      bl test     //We always want to jump back to test
 
 
 //Stuff after loop
 doneLoop:
-
-
 
 // PATRICK'S STUFF GOES HERE////////////////////////////////////////////////////
 
@@ -269,7 +270,6 @@ sub	r1, r0						// r1 = length of testArray
 bl WriteStringUART
 
 b stop
-b stop
 
 // SORT BYTE ARRAY OF INTS (WORKING)
 ldr r0, =testArray
@@ -287,30 +287,32 @@ b	stop
 // input r0 = arrayStart
 // input r1 = arrayEnd
 // output r0  = arrayStart
+
+
 sortArray:
 
-mov r2, r0      // save reference to arrayStart
+  mov r2, r0      // save reference to arrayStart
 
-loopBody:
+  loopBody:
+    ldrb r3, [r0], #1				// r3 = r0[0]; r0 += 1;
+    ldrb r4, [r0]					// r4 = r0[0];
 
-  ldrb r3, [r0], #1				// r3 = r0[0]; r0 += 1;
-ldrb r4, [r0]					// r4 = r0[0];
+    cmp r0, r1            			// end of array?
+    beq loopEnd
 
-  cmp r0, r1            			// end of array?
-  beq loopEnd
+    cmp r3, r4            			// if (r3 < r4) then array is not sorted
+    blt notSorted
 
-  cmp r3, r4            			// if (r3 < r4) then array is not sorted
-  blt notSorted
+    b loopBody
 
-  b loopBody
 
   notSorted:
-  strb r3, [r0], #-1
-  strb r4, [r0]
-  mov r0, r2          		// reset r0 to arrayStart
-  b loopBody
+    strb r3, [r0], #-1
+    strb r4, [r0]
+    mov r0, r2          		// reset r0 to arrayStart
+    b loopBody
 
-loopEnd:
+    loopEnd:
 
 mov pc, r14			 			// return
 
@@ -320,24 +322,23 @@ mov pc, r14			 			// return
 // output r2  = median value
 getMedian:
 
-mov r2, r0      // save reference to arrayStart
+  mov r2, r0      // save reference to arrayStart
 
-loopBody_:
+  loopBody_:
 
-add r0, #1						// advance array start 1 index
-sub r1, #1						// advance array end -1 index
+    add r0, #1						// advance array start 1 index
+    sub r1, #1						// advance array end -1 index
 
-  cmp r0, r1
-  bge loopEnd_					// if (start index >= end index) then found mid
+    cmp r0, r1
+    bge loopEnd_					// if (start index >= end index) then found mid
 
-  b loopBody_
+    b loopBody_
 
-loopEnd_:
+  loopEnd_:
 
 // if the array size is even, r1 will be r0 - 1
 // if the array size is odd, r1 will equal r0
 ldrb r2, [r1]
-
 mov pc, r14						// return
 
 
@@ -354,8 +355,22 @@ mov pc, r14						// return
 
 // END OF PATRICK'S STUFF //////////////////////////////////////////////////////
 
-  //NJE TODO: Print sorted list
-  //NJE TODO: Print median
+  //Print sorted list
+  ldr r0, =storedListMessage
+  mov r1, #20
+  bl WriteStringUART
+  nop
+
+    //Print values
+
+
+  //Print median
+  ldr r0, =medianMessage
+  mov r1, #15
+  bl WriteStringUART
+  nop
+
+    //Print value
 
 
   //NJE: End of program. Print "###################"
@@ -363,19 +378,6 @@ mov pc, r14						// return
   mov r1, #21
   bl WriteStringUART
   nop
-
-
-
-
-
-  //NJE: This is how to load
-  //ldr r4, =inputBuffer
-  //ldrb r5, [r4]
-  //ldrb r6, [r4, #1]
-  //mov r1, r1
-  //nop
-
-
 
 
 //##############################################################//
@@ -389,6 +391,10 @@ testArrayEnd:
 
 decToASCIBuffer:
 	.asciz "000000"
+
+atodArray:
+  .byte 0, 0, 0, 0, 0, 0, 0, 0, 0
+atodArrayEnd:
 
 inputBuffer:
   .ascii "", "", ""
@@ -409,6 +415,14 @@ wrongListSizeFormatStringEnd:
 wrongIntInput:
   .ascii "Wrong input format! Please input int from [1-100]\n\r"
 wrongIntInputEnd:
+
+storedListMessage:
+  .ascii "The sorted list is: "
+storedListMessageEnd:
+
+medianMessage:
+  .ascii "The median is: "
+medianMessageEnd:
 
 endOfRun:
   .ascii "###################\n\r"
@@ -437,3 +451,44 @@ loopNumberSizeEnd:
 
 
 .end
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO: DELETE THIS SECTION AFTER DONE. THIS IS NOTES.
+
+//NJE: This is how to load
+//ldr r4, =inputBuffer
+//ldrb r5, [r4]
+//ldrb r6, [r4, #1]
+//mov r1, r1
+//nop
+
+//NOTE: THIS WORKS NOW. THIS IS HOW TO READ FROM ARRAY
+    //I just don't know how to access individual elements.
+      //Think it has to do with the "This is how to load"
+//ldr r4, =loopNumberString
+//mov r0, r4
+//mov r1, #7
+//bl WriteStringUART
+
+//ldr r4, =loopNumberString
+//ldrb r0, [r4, #1]
+//mov r1, #6
+//bl WriteStringUART
+
+//ldr r4, =loopNumberSize
+//ldrb r1, [r4, #4]
+//bl WriteStringUART
+//nop
+//nop
+//nop
+//ENDTODO
