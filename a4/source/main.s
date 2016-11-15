bbbbc3b0f7a06b5c99c5c7a62e5b6427e9546f00
patrick.sluth <patrick.sluth@pi03-ec.cs.ucalgary.ca>
2016-11-14 21:14:07 -0700

    backup

--- a4/source/main.s
+++ a4/source/main.s
@@ -26,6 +26,11 @@ main:
 	
 	
 	
+	
+	
+	
+	
+	
 	ldr 		r0, =TetrisGrid
 	add r0, #12
 	ldr 		r1, =TetrisGridEnd
@@ -107,35 +112,46 @@ main:
 	mainLoop:
 	
 	
+	
 		// TODO: pass block into tetrisUpdateGridWithCurrentBlock
 		//		 and create an empty block on stack to clear
 	
 		// Load current block
-		ldr		r0, 		=TetrisCurrentBlock
+		ldr		r0, 		=TetrisCurrentBlockTest
 		ldr		r1, 		[r0, #0]
 		ldr		r2, 		[r0, #4]
 		ldr		r3, 		[r0, #8]
 		ldr		r4, 		[r0, #12]
 		ldr		r5,			[r0, #16]
-		ldrh	r6, 		[r0, #20]
+		ldr		r6, 		[r0, #20]
+		ldr		r7, 		[r0, #24]
 		
-		push { r0 - r6 }
+		
+		
+		push { r0 - r7 }
 	
 		str		r1, 		[r0, #8]
 		str		r2, 		[r0, #12]
 		mov		r5,			#0 // black
 		str		r5,			[r0, #16]
 	
-		pop { r0 - r6 }
+		pop { r0 - r7 }
+		
 		
 		bl tetrisUpdateGridWithCurrentBlock
 		
 		str		r3, 		[r0, #8]
 		str		r4, 		[r0, #12]
 		str		r5,			[r0, #16]
+	
 		
 		bl tetrisUpdateGridWithCurrentBlock
 		
+		bl tetrisRotateBlockTest2
+	
+	
+	
+	
 	
 		
 		bl drawTetrisGrid
@@ -302,59 +318,68 @@ tetrisUpdateGridWithCurrentBlock:
 
 	push 	{ lr }
 	
-	blockPrevX		.req r1
-	blockPrevY		.req r2
-	blockX			.req r3
-	blockY			.req r4
-	blockColor		.req r5
-	blockGridData	.req r6
+	blockPrevX			.req r1
+	blockPrevY			.req r2
+	blockX				.req r3
+	blockY				.req r4
+	blockColor			.req r5
+	blockAddress		.req r6
+	blockAddressOffset	.req r7
+	blockGridData		.req r8
 	
-	push { blockPrevX, blockPrevY, blockX, blockY, blockColor, blockGridData }
+	push { r0 - r12 }
 	
 	
 	
 	
 	
 	// Load current block
-	ldr		r0, 				=TetrisCurrentBlock
-	ldr		blockPrevX, 		[r0, #0]
-	ldr		blockPrevY, 		[r0, #4]
-	ldr		blockX, 			[r0, #8]
-	ldr		blockY, 			[r0, #12]
-	ldr		blockColor,			[r0, #16]
-	ldrh	blockGridData, 		[r0, #20]
+	ldr		r0, 					=TetrisCurrentBlockTest
+	ldr		blockPrevX, 			[r0, #0]
+	ldr		blockPrevY, 			[r0, #4]
+	ldr		blockX, 				[r0, #8]
+	ldr		blockY, 				[r0, #12]
+	ldr		blockColor,				[r0, #16]
+	ldr		blockAddress, 			[r0, #20]
+	ldr		blockAddressOffset, 	[r0, #24]
 	
-	teq		blockGridData,		#0
+	
+	
+	
+	
+	teq		blockAddress,			#0
 	beq		generateBlock
-	b 		generateBlockEnd
+	bne 	generateBlockEnd
 	
 	generateBlock:
 	
-		mov		blockX, 		#0
-		mov		blockY, 		#0
-		//	0xC440
-		//	11--
-		//	-1--
-		//	-1--
-		//	----
-		ldr		blockGridData, 	=0xC440
-		str		blockGridData,	[r0, #20]
+		mov		blockX, 			#0
+		mov		blockY, 			#0
+		ldr		blockAddress, 		=TetrisBlockA
+		str		blockAddress, 		[r0, #20]
+		mov		blockAddressOffset,	#0
+		str		blockAddressOffset,	[r0, #24]
 	
 	generateBlockEnd:
 	
+		mov		blockPrevX,			blockX
+		mov		blockPrevY,			blockY
+		str		blockPrevX,			[r0, #0]
+		str		blockPrevY,			[r0, #4]
+		
+		ldrh	blockGridData, 		[blockAddress, blockAddressOffset]
 	
-		mov		blockPrevX,	blockX
-		mov		blockPrevY,	blockY
-		str		blockPrevX,	[r0, #0]
-		str		blockPrevY,	[r0, #4]
+		// ROTATE
+		//add		blockAddressOffset, #2
+		//cmp		blockAddressOffset, #6
+		//movgt	blockAddressOffset, #0
 		
 		
-	
 		// clear previous position
 		
 	
-		i	.req r8
-		j	.req r9
+		i	.req r9
+		j	.req r10
 	
 		mov	i, #0
 		mov j, #0
@@ -371,25 +396,25 @@ tetrisUpdateGridWithCurrentBlock:
 				add blockX, i
 				add blockY, j
 				
-				mov		r10, #30	// TEMP HARDCODED ROWS
-				mul		r11, r10, blockY			// calculate tetris grid offset
+				mov		r11, #30	// TEMP HARDCODED ROWS
+				mul		r11, r11, blockY			// calculate tetris grid offset
 				add		r11, blockX
 				lsl		r11, #2
 				// R11 is the block in the tetris grid
 				pop { blockX, blockY }
 				
-				mov		r10, #4
-				mul		r10, r10, j
-				add		r10, i
+				mov		r12, #4
+				mul		r12, r12, j
+				add		r12, i
 				
 				//pop { blockX, blockY }
 				push { blockGridData }
 				
-				mov		r7, #0b1000000000000000
-				lsl		blockGridData, r10
-				and		r7, blockGridData
-				teq		r7, #0
-				movne	r7, #1
+				lsl		blockGridData, r12
+				mov		r12, #0b1000000000000000
+				and		r12, blockGridData
+				teq		r12, #0
+				movne	r12, #1
 				
 				
 				// write block
@@ -398,7 +423,7 @@ tetrisUpdateGridWithCurrentBlock:
 				add 		r0, #12
 				ldreq		blockColor, =0x000000	// black
 				str			blockColor, [r0, r11]	// color
-				//streq		r7, [r0, r11]	// color
+				//streq		r12, [r0, r11]	// color
 				pop { r0, blockColor }
 				
 				
@@ -417,8 +442,8 @@ tetrisUpdateGridWithCurrentBlock:
 			cmp 	i, #4
 			blt 	for_i_lessThan_4_loop
 			
-			.unreq			i
-			.unreq 			j
+			.unreq		i
+			.unreq 		j
 			
 	add		blockY, 			#1
 	str		blockY, 			[r0, #12]
@@ -435,17 +460,190 @@ tetrisUpdateGridWithCurrentBlock:
 	
 	
 	
-	pop { blockPrevX, blockPrevY, blockX, blockY, blockColor, blockGridData }
+	pop { r0 - r12 }
 	
 	.unreq			blockPrevX
 	.unreq			blockPrevY
 	.unreq			blockX
 	.unreq 			blockY
 	.unreq 			blockColor
+	.unreq 			blockAddress
+	.unreq 			blockAddressOffset
 	.unreq 			blockGridData
 	
 	pop 	{ lr }
 	mov		pc, lr
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+tetrisRotateBlockTest:
+
+	push 	{ lr }
+	
+	blockGridData		.req r0
+	rowBitMask			.req r1
+	row1				.req r2
+	row2				.req r3
+	row3				.req r4
+	row4				.req r5
+	
+	push { blockGridData, rowBitMask, row1, row2, row3, row4 }
+	
+	
+	
+	
+	
+	ldr		r0, 				=TetrisCurrentBlock
+	ldrh	blockGridData, 		[r0, #20]
+	
+	mov		rowBitMask, #0b1111
+	
+	and		row4, rowBitMask, blockGridData
+	lsr		blockGridData, #4
+	
+	and		row3, rowBitMask, blockGridData
+	lsr		blockGridData, #4
+	
+	and		row2, rowBitMask, blockGridData
+	lsr		blockGridData, #4
+	
+	and		row1, rowBitMask, blockGridData
+	
+	
+	
+	
+	
+	
+	
+	
+	// ROTATION (+ pi/2)
+	
+	
+	
+	
+	
+	
+	
+	// ROTATION (- pi/2)
+	
+	
+	
+	
+	
+	
+	
+	
+	pop { blockGridData, rowBitMask, row1, row2, row3, row4 }
+	
+	.unreq 				blockGridData
+	.unreq 				rowBitMask
+	.unreq				row1
+	.unreq				row2
+	.unreq				row3
+	.unreq 				row4
+	
+	pop 	{ lr }
+	mov		pc, lr	
+	
+	
+	
+	
+	
+tetrisRotateBlockTest2:
+
+	push 	{ lr }
+	
+	blockGridData		.req r0
+	blockAddress		.req r1
+	blockAddressOffset	.req r2
+	
+	push { blockGridData, blockAddress, blockAddressOffset }
+
+
+	// Load current block
+	ldr		r0, 				=TetrisCurrentBlockTest
+	
+	//generateBlock:
+	
+		ldr		blockAddress, 		=TetrisBlockA
+		str		blockAddress, 		[r0, #20]
+		mov		blockAddressOffset,	#0
+		str		blockAddressOffset,	[r0, #24]
+	
+	
+	
+	ldrh		r7, [blockAddress, blockAddressOffset]
+	
+	add		blockAddressOffset, #2
+	cmp		blockAddressOffset, #6
+	movgt	blockAddressOffset, #0
+	
+	ldrh		r7, [blockAddress, blockAddressOffset]
+	
+	add		blockAddressOffset, #2
+	cmp		blockAddressOffset, #6
+	movgt	blockAddressOffset, #0
+	
+	ldrh		r7, [blockAddress, blockAddressOffset]
+	
+	add		blockAddressOffset, #2
+	cmp		blockAddressOffset, #6
+	movgt	blockAddressOffset, #0
+	
+	ldrh		r7, [blockAddress, blockAddressOffset]
+	
+	add		blockAddressOffset, #2
+	cmp		blockAddressOffset, #6
+	movgt	blockAddressOffset, #0
+	
+	ldrh		r7, [blockAddress, blockAddressOffset]
+	
+	
+	
+	
+	// ROTATION (+ pi/2)
+	
+	
+	
+	
+	
+	
+	
+	// ROTATION (- pi/2)
+	
+	
+	
+	
+	
+	
+	
+	
+	pop { blockGridData, blockAddress, blockAddressOffset }
+	
+	.unreq 				blockGridData
+	.unreq 				blockAddress
+	.unreq				blockAddressOffset
+	
+	pop 	{ lr }
+	mov		pc, lr	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
@@ -579,11 +777,55 @@ TetrisCurrentBlock:
 	.word		0xFFABCC	// color
 	.space 		2			// blockGridData (16 bits, max block size is 4 x 4)
 	// 	EX BLOCK (check if block exists by comparing blockGridData to 0
-	//	1100
-	//	0100
-	//	0100
-	//	0000
+	
 TetrisCurrentBlockEnd:
+
+
+
+
+.align 4
+TetrisCurrentBlockTest:
+	.int		0			// prevX
+	.int		0			// prevY
+	.int		0			// x
+	.int		0			// y
+	.word		0xFFABCC	// color
+	.word 		2			// blockAddress
+	.int 		0			// blockAddressOffset (0 - 3)
+	
+TetrisCurrentBlockTestEnd:
+
+
+
+
+.align 4
+TetrisBlockA:
+	.hword		0x8E00			// 0
+	//	1---
+	//	111-
+	//	----
+	//	----
+	.hword		0x44C0			// pi/2
+	//	-1--
+	//	-1--
+	//	11--
+	//	----
+	.hword		0xE200			// pi
+	//	111-
+	//	--1-
+	//	----
+	//	----
+	.hword		0xC880			// 3pi/4
+	//	11--
+	//	1---
+	//	1---
+	//	----
+TetrisBlockEnd:
+
+
+
+
+
 
 .end
 
